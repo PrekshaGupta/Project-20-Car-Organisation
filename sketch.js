a25e005ca094873af95fcccf692a3a46e712a4ae
PrekshaGupta <69909564+PrekshaGupta@users.noreply.github.com>
2020-09-08 09:25:49 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -16,11 +16,7 @@ function setup() {
 }
 
 function draw() {
-  background(80,80,80);
-
-  if(car.collide(wall)) {
-    car.velocityX = 0;
-  }
+  background(255);
 
   if(wall.x - car.x < (car.width + wall.width)/2){
    car.velocityX = 0;
